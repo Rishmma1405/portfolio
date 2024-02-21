284375726df2d67ba4a90aa6d56a993e84a0196b
Rishmma1405 <160393688+Rishmma1405@users.noreply.github.com>
2024-02-21 20:09:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text",{
+    strings:[" Java Developer" , "Web Developer ", "Sotware Tester", "C# Developer","Dot Net Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+})
