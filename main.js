bc7314fd3c00b2405beb895942fb25255d80e9fd
Rishmma1405 <160393688+Rishmma1405@users.noreply.github.com>
2024-02-29 15:48:26 +0530

    main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text",{
-    strings:[" Java Developer" , "Web Developer ", "Sotware Tester", "C# Developer","Dot Net Developer"],
+    strings:[" Java Developer" , "Web Developer ", "Digital Marketing Excutive", "C# Developer","Dot Net Developer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
